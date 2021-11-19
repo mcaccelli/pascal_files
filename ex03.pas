2f4719e64832c105f913f32be06de4d875f74586
Mateus Caccelli <56459760+mcaccelli@users.noreply.github.com>
2021-11-18 23:40:34 -0300

    Add files via upload

---
+++ ex03.pas
@@ -0,0 +1,15 @@
+Program Lesson2_Program2;
+Var 
+	surname: String;
+ 
+Const {the reserved word 'const' is used to initialize constants}
+	name = 'Victor';
+     
+Begin
+	Write('Enter your surname:');
+	readln(surname);
+	writeln;
+	writeln;
+	Writeln('Your full name is: ',name,' ',surname);
+	Readln;
+End.          
